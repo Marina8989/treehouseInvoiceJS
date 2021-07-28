addec39fc0852a3f824ca35cdd4dea8254d0e22a
Marina8989 <marina.mill209@gmail.com>
2021-07-28 14:16:12 -0700

    invoice

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const createRowBtn = document.querySelector('#createRowBtn');
 const row = document.querySelector('.row');
-const clearBtn = docuemnt.querySelector('#clearBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const stop = document.querySelector('.stop');
+const container = document.querySelector('.container');
+const closeAlertBtn = document.querySelector('.closeAlertBtn');
 
 createRowBtn.addEventListener('click', () => {
     if(row.value == '') {
@@ -10,5 +13,17 @@ createRowBtn.addEventListener('click', () => {
 })
 
 clearBtn.addEventListener('click', () => {
-    
+    container.style.position = 'relative';
+    stop.style.display = 'block';
+    stop.style.padding = '4rem';
+    stop.style.border = '7rem solid rgba(0,0,0, .7)'
+    stop.style.position = 'absolute';
+    stop.style.top = '7.2rem';
+    stop.style.left = '12rem';
+    stop.style.background = 'rgba(250, 250, 250, .9)';
+    stop.style.zIndex = '10';
+})
+
+closeAlertBtn.addEventListener('click', () => {
+    stop.style.display = 'none';
 })
