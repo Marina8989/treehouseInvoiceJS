342bfe4d9c4f42e968ed0b5b4d9390bf89015de0
Marina8989 <marina.mill209@gmail.com>
2021-07-28 13:31:13 -0700

    invoice

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const createRowBtn = document.querySelector('#createRowBtn');
 const row = document.querySelector('.row');
+const clearBtn = docuemnt.querySelector('#clearBtn');
 
 createRowBtn.addEventListener('click', () => {
     if(row.value == '') {
        alert('Please replace the "#" with a number between 1 and 42, then try again.')
     }
-     
+    //  else add table to html 
+})
+
+clearBtn.addEventListener('click', () => {
+    
 })
